a8dc1eaaf5468544b0c847deb8de9d9c6d5be4b7
Mostafa <53868779+MostafaGhalia@users.noreply.github.com>
2024-02-23 20:14:25 +0100

    Initialization + GetTeam function

---
+++ main.py
@@ -0,0 +1,132 @@
+from requests_html import HTMLSession
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.drawing.image import Image
+from openpyxl.utils.exceptions import InvalidFileException
+import os
+import shutil
+
+s = HTMLSession()
+url_root = "https://liquipedia.net/"
+url_teams = "https://liquipedia.net/rainbowsix/Portal:Teams"
+url_tournament = "https://liquipedia.net/rainbowsix/Portal:Tournaments"
+list_tournaments = ["S-Tier_Tournaments", "A-Tier_Tournaments", "B-Tier_Tournaments", "C-Tier_Tournaments"]
+excel_file = "out/LiquipediaScraping.xlsx"
+media_folder = "media/"
+
+def create_excel_file():
+    workbook = Workbook()
+    worksheet_players = workbook.active
+    worksheet_players.title = "Players"
+    worksheet_players.append(["Flag", "Player Name", "Player Surname", "Role", "Team"])
+    workbook.save(excel_file)
+    print("Fichier Excel créé avec succès.")
+    return workbook
+
+
+try:
+    if not os.path.exists(excel_file):
+        workbook = create_excel_file()
+    else:
+        # copie le fichier excel existant
+        shutil.copyfile(excel_file, excel_file + ".bak")
+
+        # Tentez d'ouvrir le fichier existant
+        workbook = load_workbook(excel_file)
+        print("Fichier Excel chargé avec succès.")
+        if "Players" in workbook.sheetnames:
+            workbook.remove(workbook["Players"])
+            worksheet_players = workbook.create_sheet(title="Players")
+            worksheet_players.append(
+                ["Flag", "Player Name", "Player Surname", "Role", "Team"]
+            )
+except InvalidFileException:
+    # Gère le cas où le fichier est corrompu et ne peut pas être ouvert
+    print(
+        "Le fichier Excel est corrompu et ne peut pas être ouvert. Un nouveau fichier sera créé."
+    )
+    workbook = create_excel_file()
+except Exception as e:
+    # Gestion d'autres types d'erreurs potentielles
+    print(f"Une erreur inattendue est survenue: {e}")
+    # delete le fichier excel et le remplacer par le bak
+    os.remove(excel_file)
+    shutil.copyfile(excel_file + ".bak", excel_file)
+
+
+# Selectionne des feuilles
+worksheet_players = workbook["Players"]
+
+
+# Si le dossier media n'existe pas, on le crée
+if not os.path.exists(media_folder):
+    os.makedirs(media_folder)
+
+
+def main():
+    get_teams()
+
+
+def get_image_name_from_url(url):
+    return url.split("/")[-1]
+
+
+def download_image(image_url):
+    r = s.get(image_url)
+    with open(media_folder + get_image_name_from_url(image_url), "wb") as f:
+        f.write(r.content)
+
+
+def get_teams():
+    r = s.get(url_teams)
+    # Recupère la liste des régions
+    region_html_list = r.html.find("div.tabs-static li")
+    region_list = []
+    for region_html in region_html_list:
+        region = region_html.find("a", first=True).full_text
+        if region != "Overview" and region != "Players":
+            region_list.append(region)
+
+    # Recupère la liste des équipes par région
+    for region in region_list:
+        url_region = url_teams + "/" + region
+        r = s.get(url_region)
+
+        team_html_list = r.html.find("div.template-box table.wikitable")
+
+        for team_html in team_html_list:
+            team_logo = team_html.find(
+                "span.team-template-image-icon img", first=True
+            ).attrs["src"]
+            download_image(url_root + team_logo)
+
+            team_name = team_html.find("span.team-template-text", first=True).full_text
+            player_html_list = team_html.find("tr")[2:]
+            for player_html in player_html_list:
+                player_flag = player_html.find("td img")[0].attrs["src"]
+                player_surname = player_html.find("td")[0].full_text
+                player_name = player_html.find("td")[1].full_text
+                player_role = player_html.find("td")[2].full_text
+
+                # download playerflag image si elle n'existe pas
+                if not os.path.exists(
+                    media_folder + get_image_name_from_url(player_flag)
+                ):
+                    download_image(url_root + player_flag)
+
+                # Insertion dans le fichier excel
+                worksheet_players.append(
+                    ["", player_name, player_surname, player_role, team_name]
+                )
+                # insertion de l'image dans le fichier excel
+                flag = Image(media_folder + get_image_name_from_url(player_flag))
+                worksheet_players.add_image(
+                    flag, get_column_letter(1) + str(worksheet_players.max_row)
+                )
+                print(
+                    f"Joueur {player_name} {player_surname} ajouté pour l'équipe {team_name}"
+                )
+    workbook.save(excel_file)
+
+
+main()
---
+++ readme.md
@@ -0,0 +1,84 @@
+# Projet de Scrapping de Données Liquipedia Rainbow Six Siege
+
+Ce projet vise à extraire des données de Liquipedia concernant le jeu Rainbow Six Siege, en se concentrant sur les tournois, les équipes, les cartes, et les statistiques de jeu.
+
+## Objectifs
+
+- Extraire les informations sur les tournois et les équipes.
+- Récupérer les détails des matchs, y compris les cartes jouées et les opérateurs bannis.
+- Analyser les statistiques des équipes et des joueurs.
+
+## Structure du Projet
+
+### 1. Configuration de l'Environnement
+
+- Utiliser Python 3.x.
+- Installer les bibliothèques nécessaires : `requests`, `BeautifulSoup` pour le scrapping HTML, et `pandas` pour la manipulation des données.
+
+### 2. Modules du Projet
+
+#### a. Tournois & Tiers
+
+- Identifier les différents tournois et leurs niveaux (Tier 1, Tier 2, etc.).
+- Extraire les informations de base : nom, date, équipes participantes.
+
+#### b. Équipes
+
+- Récupérer les informations sur les équipes : nom, membres, classement.
+- Distinguer les équipes par Tiers si possible.
+
+#### c. Cartes
+
+- Identifier les cartes bannies lors des matchs en BO1, BO3, et BO5.
+- Extraire l'ordre de bannissement des cartes par équipe.
+
+#### d. Opérateurs
+
+- Extraire les opérateurs bannis par carte et de manière générale.
+- Distinguer entre opérateurs d'attaque et de défense bannis.
+
+### 3. Extraction et Analyse des Données
+
+- Utiliser des requêtes HTTP pour récupérer les pages web.
+- Analyser le HTML avec BeautifulSoup pour extraire les données.
+- Stocker les données extraites dans des DataFrame pandas pour analyse.
+
+### 4. Visualisation des Résultats
+
+- Créer des graphiques comparatifs pour les équipes et les statistiques sélectionnées.
+- Analyser les tendances des victoires/défaites sur les cartes, ainsi que l'efficacité en attaque et en défense.
+
+### 5. Automatisation et Mise à Jour des Données
+
+- Mettre en place des scripts pour automatiser l'extraction périodique des données.
+- Assurer la mise à jour des bases de données pour refléter les derniers tournois et matchs.
+
+## Documentation et Ressources
+
+- Documentation officielle de Python, Requests, et BeautifulSoup.
+- [Page des équipes sur Liquipedia](https://liquipedia.net/rainbowsix/Portal:Teams)
+- [Page des tournois sur Liquipedia](https://liquipedia.net/rainbowsix/Portal:Tournaments)
+- [Page des cartes sur Liquipedia](https://liquipedia.net/rainbowsix/Portal:Maps)
+
+
+/Special:RunQuery/
+https://www.mediawiki.org/wiki/Extension:Page_Forms/Creating_query_forms#Displaying_results_automatically
+
+https://liquipedia.net/rainbowsix/Special:RunQuery/MapWLOverview?_run=
+https://liquipedia.net/rainbowsix/Category:Semantic_query_templates
+https://liquipedia.net/rainbowsix/Special:RunQuery/Notability_Checker
+
+
+/rainbowsix/Portal:
+https://liquipedia.net/rainbowsix/Portal:Teams
+https://liquipedia.net/rainbowsix/Portal:Tournaments
+https://liquipedia.net/rainbowsix/Portal:Maps
+
+
+https://liquipedia.net/rainbowsix/Help:Mediawiki
+https://liquipedia.net/rainbowsix/Help:Magic_Words
+https://liquipedia.net/rainbowsix/Special:SpecialPages 
+
+## Conclusion
+
+Ce projet fournira une base solide pour analyser les tendances et les performances dans Rainbow Six Siege à partir des données de Liquipedia, offrant ainsi des insights précieux aux fans, analystes et participants des tournois.
